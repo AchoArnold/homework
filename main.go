c9a980adc04be0632b5d7850c239a1c59a43faa5
Acho Arnold <a.ewin@geophy.com>
2020-03-01 01:57:40 +0100

    Setting up the database

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/go-errors/errors"
 	"github.com/joho/godotenv"
 	"io"
 	"log"
@@ -12,6 +13,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 	bolt "go.etcd.io/bbolt"
 )
@@ -30,6 +32,11 @@ type EmailAddress struct {
 	Address string
 }
 
+type TestTakerEmail struct {
+	TestTakerId int
+	Email string
+}
+
 type Email struct {
 	Subject string
 	Body    string
@@ -42,7 +49,7 @@ type AuthRequest struct {
 
 type Error struct {
 	Err          error
-	Message      string
+	Message string
 	ApiErrorCode string
 	isApiError   bool
 }
@@ -51,18 +58,18 @@ func (err *Error) Error() string {
 	return err.Message
 }
 
-func NewError(currentError error, message string) *Error {
+func NewError(currentError error, message string) error {
 	return &Error{
 		Err:        currentError,
-		Message:    message,
+		Message:    strings.Join([]string{message, currentError.Error()}, "\n"),
 		isApiError: false,
 	}
 }
 
-func NewApiError(message string, errorCode string) *Error {
+func NewApiError(message string, errorCode string) error {
 	return &Error{
-		Err:          nil,
-		Message:      message,
+		Err:          errors.New("Api Error"),
+		Message:      strings.Join([]string{message, "Api Error"}, "\n"),
 		ApiErrorCode: errorCode,
 		isApiError:   true,
 	}
@@ -86,9 +93,9 @@ func (request *TestTakersRequest) ToMap() map[string]string {
 }
 
 type TestTakersResponse struct {
-	TestTakers []TestTakers `json:"test_takers"`
-	Total      int          `json:"total"`
-	Error *ApiError `json:"error,omitempty"`
+	TestTakers []TestTaker `json:"test_takers"`
+	Total      int         `json:"total"`
+	Error *ApiError        `json:"error,omitempty"`
 }
 type ContactInfo struct {
 	Phone        string `json:"phone"`
@@ -102,7 +109,7 @@ type ContactInfo struct {
 	Linkedin     string `json:"linkedin"`
 	ContactEmail string `json:"contact_email"`
 }
-type TestTakers struct {
+type TestTaker struct {
 	ID                    int      `json:"id"`
 	Name                  string      `json:"name"`
 	Email                 string      `json:"email"`
@@ -130,7 +137,7 @@ func init() {
 	}
 }
 
-func getAccessToken() (*AuthResponse, *Error) {
+func getAccessToken() (*AuthResponse, error) {
 	authRequest := AuthRequest{
 		Email:    os.Getenv("API_AUTH_EMAIL"),
 		Password: os.Getenv("API_AUTH_PASSWORD"),
@@ -160,7 +167,7 @@ func getAccessToken() (*AuthResponse, *Error) {
 	return &accessTokenResponse, nil
 }
 
-func getTestTakers(accessToken *AuthResponse, limit, offset int) (*TestTakersResponse, *Error) {
+func getTestTakers(accessToken *AuthResponse, limit, offset int) (*TestTakersResponse, error) {
 	testTakersInput := &TestTakersRequest{limit, offset}
 
 	request, err := createGetRequest(os.Getenv("API_TEST_TAKERS_ENDPOINT"), testTakersInput.ToMap(), accessToken)
@@ -186,18 +193,17 @@ func getTestTakers(accessToken *AuthResponse, limit, offset int) (*TestTakersRes
 	return &testTakersResponse, nil
 }
 
-func decodeJSON(variable interface{}, reader io.Reader) *Error {
+func decodeJSON(variable interface{}, reader io.Reader) error {
 	decoder := json.NewDecoder(reader)
 	err := decoder.Decode(variable)
 	if err != nil {
-		log.Printf("%s", err.Error())
 		return NewError(err, fmt.Sprintf("Cannot Decode Content into object %v", variable))
 	}
 
 	return nil
 }
 
-func createGetRequest(url string, params map[string]string, accessToken *AuthResponse) (*http.Request, *Error) {
+func createGetRequest(url string, params map[string]string, accessToken *AuthResponse) (*http.Request, error) {
 	apiRequest, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, NewError(err, "cannot create request for URL: "+url)
@@ -216,7 +222,7 @@ func createGetRequest(url string, params map[string]string, accessToken *AuthRes
 	return apiRequest, nil
 }
 
-func createPostRequest(url string, request interface{}) (*http.Request, *Error) {
+func createPostRequest(url string, request interface{}) (*http.Request, error) {
 	requestBytes, err := json.Marshal(request)
 	if err != nil {
 		return nil, NewError(err, fmt.Sprintf("cannot serialize request to json %v", request))
@@ -233,7 +239,7 @@ func createPostRequest(url string, request interface{}) (*http.Request, *Error) 
 	return apiRequest, nil
 }
 
-func doHTTPRequest(request *http.Request) (*http.Response, *Error) {
+func doHTTPRequest(request *http.Request) (*http.Response, error) {
 	client := &http.Client{}
 
 	apiResponse, err := client.Do(request)
@@ -253,84 +259,52 @@ func emailIsValid(email string) bool {
 }
 
 func handleTestTakers(db *bolt.DB) {
-	var value []byte
-	err := db.Update(func(tx *bolt.Tx) error {
-		bucket,err := tx.CreateBucketIfNotExists([]byte("config"))
-		if err != nil {
-			return err
-		}
-
-		value = bucket.Get([]byte("last_finished_at"))
-		return nil
-	})
+	accessToken, err := getAccessToken()
 	if err != nil {
-		log.Printf("%v", err)
-	}
-
-	println("were're here 1")
-
-	lastFinishedAt := 0
-	if value != nil {
-		lastFinishedAt, err = strconv.Atoi(string(value))
-		if err != nil {
-			log.Println("Error converting byte to integer", )
-		}
-	}
-
-	println("were're here 2")
-
-	accessToken, errors := getAccessToken()
-	if errors != nil {
 		log.Fatalf("Error %v", err)
 	}
 
-	var testTakers []TestTakers
-	for {
-		limit := 10
-		allNewTestTakarsFound := false
-		testTakersResponse, err := getTestTakers(accessToken, limit, 0)
+	lastFinishedAt, err := FetchLastFinishedAt(db)
+	if err != nil {
+		logError(err)
+	}
+
+	var testTakers []TestTaker
+	limit := 10
+	allNewTestTakersFound := false
+	testTakersResponse, err := getTestTakers(accessToken, limit, 0)
+	if err != nil {
+		logError(err)
+	}
+
+	if len(testTakersResponse.TestTakers) > 0 {
+		err := StoreLastFinishedAt(db, testTakersResponse.TestTakers[0].FinishedAt)
+		if err != nil {
+			logError(err)
+		}
+	}
+
+	for _, testTaker := range testTakersResponse.TestTakers {
+		if lastFinishedAt > -1  &&  testTaker.FinishedAt < lastFinishedAt {
+			allNewTestTakersFound = true
+			break
+		}
+		testTakers = append(testTakers, testTaker)
+	}
+
+	for i := 1; (float64(i) < math.Ceil(float64(testTakersResponse.Total) / float64(limit))) || allNewTestTakersFound; i++ {
+		testTakersResponse, err := getTestTakers(accessToken, limit, limit * i)
 		if err != nil {
 			log.Fatalf("Error %v", err)
 		}
 
-		if testTakersResponse.Total <= 10 {
-			break
-		}
-
 		for _, testTaker := range testTakersResponse.TestTakers {
 			if lastFinishedAt > 0  &&  testTaker.FinishedAt < lastFinishedAt {
-				allNewTestTakarsFound = true
+				allNewTestTakersFound = true
 				break
 			}
 			testTakers = append(testTakers, testTaker)
 		}
-
-		if allNewTestTakarsFound || testTakersResponse.Total < limit {
-			break
-		}
-
-		for i := 1; float64(i) < math.Ceil(float64(testTakersResponse.Total) / float64(limit)); i++ {
-			testTakersResponse, err := getTestTakers(accessToken, limit, limit * i)
-			if err != nil {
-				log.Fatalf("Error %v", err)
-			}
-
-			for _, testTaker := range testTakersResponse.TestTakers {
-				if lastFinishedAt > 0  &&  testTaker.FinishedAt < lastFinishedAt {
-					allNewTestTakarsFound = true
-					break
-				}
-				testTakers = append(testTakers, testTaker)
-			}
-
-			if allNewTestTakarsFound {
-				break
-			}
-
-		}
-
-		break
-
 	}
 
 	log.Printf("Total Test Takers = %d", len(testTakers))
@@ -338,7 +312,22 @@ func handleTestTakers(db *bolt.DB) {
 	var email logSender
 	for _, testTaker := range testTakers {
 		if testTaker.Percent >= 80 && !testTaker.IsDemo && emailIsValid(testTaker.ContactInfo.ContactEmail) {
-			err := email.Send(
+			testTakerEmail, err := FetchEmailForTestTaker(db, testTaker)
+			if err != nil {
+				logError(err)
+				continue
+			}
+
+			if testTakerEmail != nil {
+				continue
+			}
+
+			testTakerEmail = &TestTakerEmail{
+				TestTakerId: testTaker.ID,
+				Email:       testTaker.Email,
+			}
+
+			err = email.Send(
 				EmailAddress{Name: os.Getenv("MAIL_FROM"), Address: os.Getenv("MAIL_FROM_EMAIL")},
 				EmailAddress{Name: testTaker.ContactInfo.FullName, Address: testTaker.ContactInfo.ContactEmail},
 				Email{
@@ -348,33 +337,169 @@ func handleTestTakers(db *bolt.DB) {
 			)
 
 			if err != nil {
-				log.Println("Error sending email")
-			}
-
-			err = Store(db, "emails_sent", strconv.Itoa(testTaker.ID), "true")
-			if err != nil {
-				log.Printf("%v", err)
+				err := StoreFailedTestTakerEmails(db, *testTakerEmail)
+				if err != nil {
+					logError(err, "could not send test taker email")
+				}
+			} else {
+				err = StoreTestTakerEmail(db, *testTakerEmail)
+				if err != nil {
+					logError(err)
+				}
 			}
 		}
 	}
-
-
 }
 
-func Store(db *bolt.DB, bucket string, key string, value string) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		bucket, err := tx.CreateBucketIfNotExists([]byte(bucket))
-		if err != nil {
+func StoreLastFinishedAt(db *bolt.DB, timestamp int) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("config"))
+		if err == nil {
 			return err
 		}
 
-		err = bucket.Put([]byte(key), []byte(value))
+		if bucket == nil {
+			return errors.New("config bucket does not exist")
+		}
+
+		err = bucket.Put([]byte("last_finished_at"), []byte(strconv.Itoa(timestamp)))
 		if err != nil {
 			return err
 		}
 
 		return nil
 	})
+
+	if err != nil {
+		return NewError(err,"could not store last finished at")
+	}
+
+	return nil
+}
+
+func FetchLastFinishedAt(db *bolt.DB) (int, error) {
+	var dbData []byte
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("config"))
+		if bucket == nil {
+			return errors.New("config bucket does not exist")
+		}
+
+		dbData = bucket.Get([]byte("last_finished_at"))
+		if dbData == nil {
+			return errors.New("invalid data in bucket")
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return -1, nil
+	}
+
+	timestamp, err := strconv.Atoi(string(dbData))
+	if err != nil {
+		return -1, NewError(err, fmt.Sprintf("Could not convert bytes %s into int", string(dbData)))
+	}
+
+	return timestamp,nil
+}
+
+// Depending on how we want to handle critical errors, Errors which reach thi function are critical and affect the flow
+// of the application so they should be looked into immediately.
+func logError(err error, parameters ...interface{}) {
+	log.Println(err.Error(), parameters)
+}
+
+
+func JsonEncode(itemToEncode interface{}) ([]byte, error) {
+	encodedObject, err := json.Marshal(itemToEncode)
+	if err != nil {
+		return nil, NewError(err, fmt.Sprintf("could not marshal object to json: %v", itemToEncode))
+	}
+
+	return encodedObject, nil
+}
+
+func StoreFailedTestTakerEmails(db *bolt.DB, taker TestTakerEmail) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("failed_test_taker_emails"))
+		if err != nil {
+			return NewError(err, "Cannot create bucket 'failed_test_taker_emails'")
+		}
+
+		testTakerEmailAsBytes, err := JsonEncode(taker)
+		if err != nil {
+			return NewError(err, fmt.Sprintf("could not marshal test taker email %v", taker))
+		}
+
+		err = bucket.Put([]byte(strconv.Itoa(taker.TestTakerId)), testTakerEmailAsBytes)
+		if err != nil {
+			return NewError(err, fmt.Sprintf("could not save failed test taker with ID %d", taker.TestTakerId))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return NewError(err, "Could not store failed test taker email")
+	}
+
+	return nil
+}
+
+func StoreTestTakerEmail(db *bolt.DB, taker TestTakerEmail) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists([]byte("test_taker_email"))
+		if err != nil {
+			return NewError(err, "Cannot create bucket 'test_taker_email'")
+		}
+
+		testTakerEmailAsBytes, err := JsonEncode(taker)
+		if err != nil {
+			log.Println(err.Error())
+			return NewError(err, fmt.Sprintf("could not marshal test taker email %v", taker))
+		}
+
+		err = bucket.Put([]byte(strconv.Itoa(taker.TestTakerId)), testTakerEmailAsBytes)
+		if err != nil {
+			return NewError(err, fmt.Sprintf("could not save test taker with ID %d", taker.TestTakerId))
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return NewError(err, "Could not store test taker email")
+	}
+
+	return nil
+}
+
+func FetchEmailForTestTaker(db *bolt.DB, testTaker TestTaker) (*TestTakerEmail, error) {
+	var testTakerEmailAsBytes []byte
+
+	_ = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("test_taker_email"))
+		if bucket == nil {
+			return nil
+		}
+
+		testTakerEmailAsBytes = bucket.Get([]byte(strconv.Itoa(testTaker.ID)))
+		return nil
+	})
+
+	if testTakerEmailAsBytes == nil {
+		return nil,nil
+	}
+
+	var testTakerEmail TestTakerEmail
+	err := decodeJSON(&testTakerEmail, bytes.NewBuffer(testTakerEmailAsBytes))
+	if err != nil {
+		return nil, NewError(err, fmt.Sprintf("cannot unmartial bytes '%s' into TestTakerEmail struct", string(testTakerEmailAsBytes)))
+	}
+
+	return &testTakerEmail, nil
 }
 
 func main() {
