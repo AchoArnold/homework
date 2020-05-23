0ba4303bedc335f45ea5c35f34779f4f01f69894
Acho Arnold <a.ewin@geophy.com>
2020-05-23 14:38:32 +0200

    Added new functions

--- services/json/json_encoder_decoder.go
+++ services/json/json_encoder_decoder.go
@@ -15,6 +15,26 @@ func JsonEncode(itemToEncode interface{}) ([]byte, error) {
 	return encodedObject, nil
 }
 
+func JsonToStringMap(value interface{})(result map[string]string, err error)  {
+	encoded, err := JsonEncode(value)
+	if err != nil {
+		return result, err
+	}
+
+	err = json.Unmarshal(encoded, &result)
+	return result, err
+}
+
+func ToInterfaceMap(value interface{})(result map[string]interface{}, err error)  {
+	encoded, err := JsonEncode(value)
+	if err != nil {
+		return result, err
+	}
+
+	err = json.Unmarshal(encoded, &result)
+	return result, err
+}
+
 func JsonDecode(variable interface{}, reader io.Reader) error {
 	decoder := json.NewDecoder(reader)
 	err := decoder.Decode(variable)
